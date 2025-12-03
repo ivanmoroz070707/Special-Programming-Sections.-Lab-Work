3be0dc5e9f13fb870d01c1956007d3ca9d21091f
Ivan Moroz <ivanmoroz070707@gmail.com>
2025-12-03 15:52:31 +0200

    New Lab 3 6

--- Lab_3/build.sh
+++ Lab_3/build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# Стоп при любой ошибке
+
 set -e
 
-# Создаем папку build если её нет
+
 mkdir -p build
 cd build
 
-# Генерация проекта
+
 cmake ..
 
-# Сборка
+
 make -j$(nproc)
 
 echo "Build finished successfully!"
--- Lab_3/src/Display.cpp
+++ Lab_3/src/Display.cpp
@@ -1,1 +1,9 @@
 #include "Display.hpp"
+
+Display::Display(const std::string &winName) : windowName(winName) {
+    cv::namedWindow(windowName, cv::WINDOW_AUTOSIZE);
+}
+
+void Display::show(const cv::Mat &frame) {
+    cv::imshow(windowName, frame);
+}
